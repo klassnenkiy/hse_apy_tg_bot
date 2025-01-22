d4852774ca3c6226597d6f1c1ff2db7c65c6782f
Stanislav Tyulyagin <klassnenkiy@gmail.com>
2025-01-22 23:20:21 +0300

    final

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN pip install -r requirements.txt
 
 COPY . .
 
-CMD ["python", "bot1.py"]
+CMD ["python", "bot.py"]
--- config.py
+++ config.py
@@ -5,6 +5,21 @@ load_dotenv()
 
 API_TOKEN = os.getenv("API_TOKEN")
 OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
+NUTRITIONIX_API_KEY = os.getenv("NUTRITIONIX_API_KEY")
+NUTRITIONIX_APP_ID = os.getenv("NUTRITIONIX_APP_ID")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 if not API_TOKEN:
     raise ValueError("Переменная окружения API_TOKEN не установлена!")
+
+if not OPENWEATHER_API_KEY:
+    raise ValueError("Переменная окружения OPENWEATHER_API_KEY не установлена!")
+
+if not NUTRITIONIX_API_KEY:
+    raise ValueError("Переменная окружения NUTRITIONIX_API_KEY не установлена!")
+
+if not NUTRITIONIX_APP_ID:
+    raise ValueError("Переменная окружения NUTRITIONIX_APP_ID не установлена!")
+
+if not WEBHOOK_URL:
+    raise ValueError("Переменная окружения WEBHOOK_URL не установлена!")
